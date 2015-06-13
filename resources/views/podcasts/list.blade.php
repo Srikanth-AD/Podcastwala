54a69ea94d2aa6f6d8fb1c11c5729727ac2f1925
Srikanth AD <adsrikanth@gmail.com>
2015-06-13 13:20:31 -0400

    Css changes

--- resources/views/podcasts/list.blade.php
+++ resources/views/podcasts/list.blade.php
@@ -55,10 +55,8 @@
         @endforeach
 
         @if($items)
-          <div class="row">
-            <div class="col-md-9">
+          <div class="row container-fluid">
               <?php echo $items->render() ?>
-            </div>
           </div>
         @endif
 
