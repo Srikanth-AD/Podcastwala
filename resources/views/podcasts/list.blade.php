f6987dc45c80a735c47430d427c3dbe3f7a84140
Srikanth AD <adsrikanth@gmail.com>
2015-06-13 12:15:44 -0400

    minor HTML/CSS changes

--- resources/views/podcasts/list.blade.php
+++ resources/views/podcasts/list.blade.php
@@ -56,7 +56,7 @@
 
         @if($items)
           <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-6">
               <?php echo $items->render() ?>
             </div>
           </div>
