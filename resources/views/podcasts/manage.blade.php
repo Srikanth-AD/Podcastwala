22b63a1751ca84c29990ea5c882fa6949ca8f548
Srikanth AD <adsrikanth@gmail.com>
2015-06-13 12:59:38 -0400

    updated html/css for manage feeds page

--- resources/views/podcasts/manage.blade.php
+++ resources/views/podcasts/manage.blade.php
@@ -2,41 +2,48 @@
 
 @section('content')
 <div class="container-fluid main container-podcast-manage">
-    <div class="col-md-6">
-        <h3>Manage Podcast Feeds</h3>
+    <div class="col-md-9">
+        <h3 class="page-title">Manage Podcast Feeds</h3>
         <hr/>
         @if(DB::table('podcasts')->where('user_id','=',Auth::user()->id)->count() > 0)
-             <table class="table">
-                <thead>
-                    <tr>
-                        <th>Added on</th>
-                        <th>Podcast Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach(DB::table('podcasts')->where('user_id','=',Auth::user()->id)->get() as $cast)
-                        <tr>
-                            <td>{{ date('F d, Y', strtotime($cast->created_at)) }}</td>
-                            <td>{{$cast->name}}</td>
-                            <td><button class="feed-delete" data-feed-machine-name="{{$cast->machine_name}}" type="button" class="btn btn-default">Delete</button></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
-
-        <h4>Add a podcast feed</h4>
-        {!! Form::model($podcast = new \App\Podcast, ['method' =>'POST','action' => ['PodcastController@add']]) !!}
-            <div class="form-group">
-                {!! Form::text('feed_url', null, 
-                ['class' => 'form-control','required','placeholder' => 'Enter a Podcast Feed Url here: http://example.com/feed']) !!}
-            </div>
-            
-            <div class="form-group">
-                {!! Form::submit('Add Feed', ['class' => 'btn btn-primary']) !!}
-            </div>
-        {!! Form::close() !!}     
-    </div>   
+            @foreach(DB::table('podcasts')->where('user_id','=',Auth::user()->id)->get() as $cast)
+                <div class="col-md-4">
+                    <div class="podcast-container">
+                        <span class="podcast-added-on">Added on {{ date('F d, Y', strtotime($cast->created_at)) }}</span>
+                        <h4 class="podcast-title">{{$cast->name}}</h4>
+                        <a target="_blank" href="{{$cast->feed_url}}">
+                            <img class="podcast-thumbnail" width="100" height="100" 
+                             src="{{asset($cast->feed_thumbnail_location)}}" />
+                        </a>
+                        <br/>
+                        <div class="podcast-action-list">
+                            <ul class="list-inline">
+                                <li class='feed-delete' data-feed-machine-name="{{$cast->machine_name}}">
+                                    <img width="20" height="20" alt="Delete" 
+                                    src="{{asset('css/icons/ic_clear_white_36dp.png')}}" /> Delete
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        @endif  
+    </div>
+    <div class="row container-fluid">
+        <div class="col-md-6">
+            <h4 class="section-title">Add a podcast feed</h4>
+            {!! Form::model($podcast = new \App\Podcast, ['method' =>'POST','action' => ['PodcastController@add']]) !!}
+                <div class="form-group">
+                    {!! Form::text('feed_url', null, 
+                    ['class' => 'form-control','required','placeholder' => 'Enter a Podcast Feed Url here: http://example.com/feed']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::submit('Add Feed', ['class' => 'btn btn-primary']) !!}
+                </div>
+            {!! Form::close() !!} 
+        </div> 
+    </div>
 </div>
 @stop
 @section('js-footer')
